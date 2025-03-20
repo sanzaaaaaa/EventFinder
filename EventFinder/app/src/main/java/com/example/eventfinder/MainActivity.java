2c29c3b6d6b0dbcbb025f8f08b5d722da9bca5b2
Carol04k <acasapaola11@gmail.com>
2025-03-20 14:04:27 +0100

    .

--- EventFinder/app/src/main/java/com/example/eventfinder/MainActivity.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.ListView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
