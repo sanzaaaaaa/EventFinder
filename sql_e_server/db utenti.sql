da8de1f7108f9faa2a6d28dea9162dea9bace364
Carol04k <acasapaola11@gmail.com>
2025-03-24 17:07:07 +0100

    creata tabella Eventi in database

--- sql_e_server/db utenti.sql
+++ sql_e_server/db utenti.sql
@@ -13,3 +13,15 @@ email VARCHAR (200) UNIQUE NOT NULL,
 data_di_nascita date NOT NULL,
 password_hash VARCHAR(255) NOT NULL);
 select * from utenti;
+
+create table eventi(
+id INT AUTO_INCREMENT  PRIMARY KEY,
+titolo VARCHAR (200) NOT NULL,
+data_evento VARCHAR (200) NOT NULL,
+luogo VARCHAR (200) UNIQUE NOT NULL,
+categoria varchar(200) NOT NULL,
+info_evento VARCHAR(255) NOT NULL,
+info_artista varchar(200) not null
+);
+
+select * from utenti;
