f36877c70426b348ecfb79aec75c90fcdb779ff0
sanzaaaaaa <59212173+sanzaaaaaa@users.noreply.github.com>
2025-03-25 23:37:52 +0100

    ho tolto alcuni import inutili

--- EventFinder/app/src/main/java/com/example/eventfinder/fragments/FiltriDialogFragment.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/fragments/FiltriDialogFragment.java
@@ -1,23 +1,26 @@
 package com.example.eventfinder.fragments;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+
 import android.widget.Button;
-import android.widget.DatePicker;
+
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.example.eventfinder.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+
 
 public class FiltriDialogFragment extends DialogFragment {
 
@@ -29,19 +32,17 @@ public class FiltriDialogFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_filtri_dialog, container, false);
 
 
+
         SeekBar seekBar = view.findViewById(R.id.seekbar_prezzo);
         TextView txtPrezzo = view.findViewById(R.id.txt_prezzo);
 
-
         seekBar.setMax(35);
         seekBar.setProgress(0);
-
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int prezzoMassimo = 350 - (progress * 10);
-
 
                 txtPrezzo.setText("Prezzo massimo: " + prezzoMassimo);
             }
@@ -53,34 +54,25 @@ public class FiltriDialogFragment extends DialogFragment {
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-
         Spinner categoriaSpinner = view.findViewById(R.id.spinner_categoria);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.categorie_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categoriaSpinner.setAdapter(adapter);
 
-
-
-
         Button btnApplica = view.findViewById(R.id.btn_applica);
         btnApplica.setOnClickListener(v -> {
             String prezzo = txtPrezzo.getText().toString(); // Leggi il prezzo
 
-
-
-
             dismiss();
         });
 
-
         Button btnReset = view.findViewById(R.id.btn_reset);
         btnReset.setOnClickListener(v -> {
-
             seekBar.setProgress(0);
             txtPrezzo.setText("Prezzo massimo: 350");
         });
 
         return view;
     }
-    }
+}
