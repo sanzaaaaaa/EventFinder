dd4bcb6fed56dbb3fa1f04296bf52404cad86a83
Carol04k <acasapaola11@gmail.com>
2025-03-25 17:48:43 +0100

    tolti i cuori dalla Home e messi nei InfoEventi

--- EventFinder/app/src/main/java/com/example/eventfinder/InfoEventi.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/InfoEventi.java
@@ -2,6 +2,7 @@ package com.example.eventfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.eventfinder.fragments.HomeFragment;
 
 public class InfoEventi extends AppCompatActivity {
+    private boolean isFilled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +18,25 @@ public class InfoEventi extends AppCompatActivity {
         setContentView(R.layout.activity_info_eventi);
 
         ImageButton indietro = findViewById(R.id.btnBack);
+        ImageButton iconaPreferiti = findViewById(R.id.iconapreferitiInfoEvento);
 
         indietro.setOnClickListener(v -> {
             Intent amici = new Intent(InfoEventi.this, MainActivity.class);
             startActivity(amici);
         });
 
+        iconaPreferiti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFilled) {
+                    iconaPreferiti.setImageResource(R.drawable.ic_pref_filled);
+                } else {
+                    iconaPreferiti.setImageResource(R.drawable.ic_pref_not_filled);
+                }
 
+                isFilled =! isFilled;
+            }
+        });
 
     }
 }
