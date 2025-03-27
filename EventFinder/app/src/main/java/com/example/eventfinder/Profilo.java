f0f961c10c51c13434673d3e206ec33a24bb9b66
Carol04k <acasapaola11@gmail.com>
2025-03-27 16:09:11 +0100

    aggiunta la classe amici nella sottocartella modelli e modificato il nome da Amici a AmiciActivity

--- EventFinder/app/src/main/java/com/example/eventfinder/Profilo.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/Profilo.java
@@ -2,22 +2,11 @@ package com.example.eventfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import android.os.Bundle;
-import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.stream.DoubleStream;
 
 public class Profilo extends AppCompatActivity {
 
@@ -41,7 +30,7 @@ public class Profilo extends AppCompatActivity {
 
 
         sezAmici.setOnClickListener(v -> {
-            Intent amici = new Intent(Profilo.this, Amici.class);
+            Intent amici = new Intent(Profilo.this, AmiciActivity.class);
             startActivity(amici);
         });
 
---
+++ EventFinder/app/src/main/java/com/example/eventfinder/modelli/Amici.java
@@ -0,0 +1,4 @@
+package com.example.eventfinder.modelli;
+
+public class Amici {
+}
