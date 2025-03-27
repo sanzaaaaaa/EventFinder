5879b2bf7b8778915cfb96ba2ba2585833697a9d
Carol04k <acasapaola11@gmail.com>
2025-03-27 16:52:31 +0100

    tolto degli attributi dalla classe Amici

--- EventFinder/app/src/main/java/com/example/eventfinder/modelli/Amici.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/modelli/Amici.java
@@ -3,16 +3,10 @@ package com.example.eventfinder.modelli;
 public class Amici {
     private String nome;
     private String cognome;
-    private String email;
-    private String dataDiNascita;
-    private String password;
 
-    public Amici(String nome, String cognome, String email, String dataDiNascita, String password) {
+    public Amici(String nome, String cognome) {
         this.nome = nome;
         this.cognome = cognome;
-        this.email = email;
-        this.dataDiNascita = dataDiNascita;
-        this.password = password;
     }
 
     public String getNome() {
@@ -29,29 +23,5 @@ public class Amici {
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDataDiNascita() {
-        return dataDiNascita;
-    }
-
-    public void setDataDiNascita(String dataDiNascita) {
-        this.dataDiNascita = dataDiNascita;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
