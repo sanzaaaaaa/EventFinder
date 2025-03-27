a2215699defca9f9ae7e51f657f58f55f8687047
Carol04k <acasapaola11@gmail.com>
2025-03-27 12:57:54 +0100

    modifiche db

--- sql_e_server/db utenti.sql
+++ sql_e_server/db utenti.sql
@@ -5,31 +5,41 @@ create schema if not exists eventfinder;
 
 use eventfinder;
 
-create table Utenti(
+ALTER TABLE Utenti CHANGE password_hash password VARCHAR(255) NOT NULL;
+
+create table utenti(
 id INT AUTO_INCREMENT  PRIMARY KEY,
 nome VARCHAR (200) NOT NULL,
 cognome VARCHAR (200) NOT NULL,
 email VARCHAR (200) UNIQUE NOT NULL,
 data_di_nascita date NOT NULL,
-password_hash
-
- VARCHAR(255) NOT NULL);
-select * from utenti;
+password VARCHAR(255) NOT NULL);
 
 create table eventi(
 id INT AUTO_INCREMENT  PRIMARY KEY,
 titolo VARCHAR (200) NOT NULL,
 data_evento VARCHAR (200) NOT NULL,
 luogo VARCHAR (200) UNIQUE NOT NULL,
-categoria varchar(200) NOT NULL,
+info_evento VARCHAR(255) NOT NULL,
+info_artista varchar(200) not null
+);
+
+create table partecipare(
+	id INT auto_increment primary key,
+    utente_id int,
+    evento_id int,
+    foreign key (utente_id) references utenti(id),
+    foreign key (evento_id) references eventi(id)
+);
+
+create table artisti(
+id INT AUTO_INCREMENT  PRIMARY KEY,
+nome VARCHAR (200) NOT NULL,
+data_evento VARCHAR (200) NOT NULL,
+luogo VARCHAR (200) UNIQUE NOT NULL,
 info_evento VARCHAR(255) NOT NULL,
 info_artista varchar(200) not null
 );
 
 
-
-use eventfinder;
-
-
-ALTER TABLE Utenti CHANGE password_hash password VARCHAR(255) NOT NULL;
 
