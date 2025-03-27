2b3ba40d64d29a5c790942c29ac8ed4fa1f62526
mazzone2261 <matteo.zappa@itsrizzoli.it>
2025-03-27 15:12:39 +0100

    inizio implementazione per aggiungere gli utenti nella lista amici

--- EventFinder/app/src/main/java/com/example/eventfinder/modelli/ApiService.java
+++ EventFinder/app/src/main/java/com/example/eventfinder/modelli/ApiService.java
@@ -1,7 +1,10 @@
 package com.example.eventfinder.modelli;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 
@@ -11,5 +14,8 @@ public interface ApiService {
 
     @POST("/login")
     Call<Void> loginUser(@Body Utente user);
+
+    @GET("/get_users")
+    Call<List<Utente>> getUsers();
 
 }
